2782fc6c30c2a91068eca41bc9045e1761216822
Scott Brown <link1900@gmail.com>
2012-07-15 17:21:21 +1000

    -added jquery/datatable/bootstrap/underscore -added greyhounds, races, and rankings stub services -added table display of greyhounds

--- app.js
+++ app.js
@@ -25,8 +25,77 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index', { title: 'AGRA Ranker' });
+});
+
+app.get('/greyhound', function(req, res){
+  res.render('greyhound', { title: 'AGRA Ranker' });
+});
+
+app.get('/api/greyhounds', function(req, res){
+  res.send({ "greyhounds" : [{"name" : "dog1", "sire": "dog1Father", "dam" : "dog1Mother"},
+  			{"name" : "dog2", "sire": "dog1Father", "dam" : "dog1Mother"},
+  			{"name" : "dog3", "sire": "dog1Father", "dam" : "dog1Mother"}
+  	]});
+});
+
+app.get('/race', function(req, res){
+  res.render('race', { title: 'AGRA Ranker' });
+});
+
+app.get('/api/races', function(req, res){
+  res.send({ "races" : [{"name" : "Race 1", "date": new Date().toDateString, "group" : "Group 1", "raceLength" : "500", "isNoRace" : "false",
+  						"placings" : [ {"place":"1", "greyhound": "dog1"},
+  									   {"place":"2", "greyhound": "dog2"},
+  									   {"place":"3", "greyhound": "dog3"},
+  									   {"place":"4", "greyhound": "dog4"},
+  									   {"place":"5", "greyhound": "dog5"},
+  									   {"place":"6", "greyhound": "dog6"},
+  									   {"place":"7", "greyhound": "dog7"},
+  									   {"place":"8", "greyhound": "dog8"}
+  									]
+  						},
+  			{"name" : "Race 2", "date": new Date().toDateString, "group" : "Group 2", "raceLength" : "500", "isNoRace" : "false",
+  						"placings" : [ {"place":"1", "greyhound": "dog1"},
+  									   {"place":"2", "greyhound": "dog2"},
+  									   {"place":"3", "greyhound": "dog3"},
+  									   {"place":"4", "greyhound": "dog4"},
+  									   {"place":"5", "greyhound": "dog5"},
+  									   {"place":"6", "greyhound": "dog6"},
+  									   {"place":"7", "greyhound": "dog7"},
+  									   {"place":"8", "greyhound": "dog8"}
+  									]
+  						},
+  			{"name" : "Race 3", "date": new Date().toDateString, "group" : "Group 3", "raceLength" : "715", "isNoRace" : "false",
+  						"placings" : [ {"place":"1", "greyhound": "dog1"},
+  									   {"place":"2", "greyhound": "dog2"},
+  									   {"place":"3", "greyhound": "dog3"},
+  									   {"place":"4", "greyhound": "dog4"},
+  									   {"place":"5", "greyhound": "dog5"},
+  									   {"place":"6", "greyhound": "dog6"},
+  									   {"place":"7", "greyhound": "dog7"},
+  									   {"place":"8", "greyhound": "dog8"}
+  									]
+  						}
+  	]});
+});
+
+app.get('/rule', function(req, res){
+  res.render('rule', { title: 'AGRA Ranker' });
+});
+
+app.get('/api/rankings', function(req, res){
+  res.send({ "rankings" : [{"rank" : "1", "greyhound": "dog1", "points" : "80"},
+  			{"rank" : "2", "greyhound": "dog2", "points" : "50"},
+  			{"rank" : "3", "greyhound": "dog3", "points" : "30"}
+  	]});
+});
+
+app.get('/todo', function(req, res){
+  res.render('todo', { title: 'AGRA Ranker' });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("Node server started at "+ new Date() + " and listening on port " + app.get('port'));
 });
