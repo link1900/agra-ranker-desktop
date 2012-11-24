242680c117ee046c8345581dfe007645f673402e
Scott Brown <link1900@gmail.com>
2012-11-24 20:14:48 +1100

    Added more styling added races added mongolab support got greyhounds reading from mongo lab added a footer added todo added favicon cleaned up heading

--- js/core.js
+++ js/core.js
@@ -1,108 +1,50 @@
-var core = angular.module('core', []).config(function($routeProvider) {
+var app = angular.module('core', ['mongolabResource']).config(function($routeProvider) {
 	$routeProvider.when('/', {
 		controller : RankingsCtrl,
 		templateUrl : 'rankings.html'
 	}).when('/greyhound', {
 		controller : GreyhoundCtrl,
 		templateUrl : 'greyhound.html'
+	}).when('/race', {
+		controller : RaceCtrl,
+		templateUrl : 'race.html'
+	}).when('/todo', {
+		controller : AdminCtrl,
+		templateUrl : 'todo.html'
+	}).when('/admin', {
+		controller : AdminCtrl,
+		templateUrl : 'admin.html'
 	}).otherwise({
 		redirectTo : '/'
 	});
 });
 
+app.constant('API_KEY', '50acb83ee4b0df68c47a0e3b');
+app.constant('DB_NAME', 'agra');
 
-core.directive('myTable', function() {
-        return function(scope, element, attrs) {
+app.factory('GreyhoundDao', function($mongolabResource) {
+    return $mongolabResource('greyhounds');
+});
 
-            // apply DataTable options, use defaults if none specified by user
-            var options = {};
-            if (attrs.myTable.length > 0) {
-                options = scope.$eval(attrs.myTable);
-            } else {
-                options = {
-                    "bStateSave": true,
-                    "iCookieDuration": 2419200, /* 1 month */
-                    "bJQueryUI": true,
-                    "bPaginate": false,
-                    "bLengthChange": false,
-                    "bFilter": false,
-                    "bInfo": false,
-                    "bDestroy": true
-                };
-            }
-
-            // Tell the dataTables plugin what columns to use
-            // We can either derive them from the dom, or use setup from the controller           
-            var explicitColumns = [];
-            element.find('th').each(function(index, elem) {
-                explicitColumns.push($(elem).text());
-            });
-            if (explicitColumns.length > 0) {
-                options["aoColumns"] = explicitColumns;
-            } else if (attrs.aoColumns) {
-                options["aoColumns"] = scope.$eval(attrs.aoColumns);
-            }
-
-            // aoColumnDefs is dataTables way of providing fine control over column config
-            if (attrs.aoColumnDefs) {
-                options["aoColumnDefs"] = scope.$eval(attrs.aoColumnDefs);
-            }
-
-            // apply the plugin
-            var dataTable = element.dataTable(options);
-
-            // watch for any changes to our data, rebuild the DataTable
-            scope.$watch(attrs.aaData, function(value) {
-                var val = value || null;
-                if (val) {
-                    dataTable.fnClearTable();
-                    dataTable.fnAddData(scope.$eval(attrs.aaData));
-                }
-            });
-        };
-    });
+app.factory('RaceDao', function($mongolabResource) {
+    return $mongolabResource('races');
+});
 
 function RankingsCtrl($scope){
 	$scope.rankings = [];
-	
-	
 }
 
-function GreyhoundCtrl($scope, Project) {
-	$scope.greyhounds = [];
-	
-	$scope.tblData = [
-            ["Webber", "Adam"]
-        ];
-
-        // not mandatory, here as an example
-        $scope.tblColumns = [
-            { "sTitle": "Surname" },
-            { "sTitle": "First Name" }
-        ];
-
-        // not mandatory, here as an example
-        $scope.columnDefs = [{ "bSortable": false, "aTargets": [1] }];
-    
-        // not mandatory, you can use defaults in directive        
-        $scope.overrideOptions = {
-            "bStateSave": true,
-            "iCookieDuration": 2419200, /* 1 month */
-            "bJQueryUI": true,
-            "bPaginate": false,
-            "bLengthChange": false,
-            "bFilter": false,
-            "bInfo": false,
-            "bDestroy": true
-        };
-        
-                // we pretend that we have received new data from somewhere (eg a search)        
-        $scope.addData = function(){
-            //$scope.tblData.push(["jones", "henry"]); // BUG? Angular doesn't pick this up
-            $scope.counter = $scope.counter+1;
-            var existing = $scope.tblData.slice();
-            existing.push([$scope.counter, $scope.counter*2]);
-            $scope.tblData = existing;
-        }
-        $scope.counter = 0
+function GreyhoundCtrl($scope,GreyhoundDao) {
+	$scope.greyhounds = GreyhoundDao.query();
 }
+
+function RaceCtrl($scope,RaceDao) {
+	$scope.races = RaceDao.query();
+}
+
+function AdminCtrl($scope, GreyhoundDao){
+	$scope.addGreyhounds = function(){
+		
+	}
+}
+
